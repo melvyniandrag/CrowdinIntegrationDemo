9fb3ec30e839971ab5debf94bcada68f038fc49e
melvyn drag <melvyniandrag@gmail.com>
2022-10-14 13:56:54 -0400

    First commit

---
+++ CrowdinIntegrationDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CrowdinIntegrationDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CrowdinIntegrationDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CrowdinIntegrationDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
